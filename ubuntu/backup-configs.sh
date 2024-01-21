d2915c89425d4c8fbd6eb7ab9a17a6666b2ccd0e
deforris <jesse.muniz@deforris.com>
2024-01-21 12:49:49 -0500

    Adding alacritty to backup

--- ubuntu/backup-configs.sh
+++ ubuntu/backup-configs.sh
@@ -1,6 +1,9 @@
 echo incomplete
 echo "********** Deforris - Ubuntu Desktop Config Backup **********"
 echo
+echo "********** Backing up alacritty. **********"
+cp ~/.config/alacritty/alacritty.toml ~/dotfiles/ubuntu/.config/alacritty/ -v
+echo
 echo "********** Backing up sway. **********"
 cp ~/.config/sway/config ~/dotfiles/ubuntu/.config/sway/ -v
 cp ~/.config/sway/bar.sh ~/dotfiles/ubuntu/.config/sway/ -v
